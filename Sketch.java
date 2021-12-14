3ce5cb1a4065b10b1e09b5f5e24e5c33ff5be688
Ahastan <ahastan.surees23@ycdsbk12.ca>
2021-12-14 12:11:36 -0500

    test push

--- Sketch.java
+++ Sketch.java
@@ -4,7 +4,7 @@ public class Sketch extends PApplet {
 	
 	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Called once at the beginning of execution, put your size all in this method	
    */
   public void settings() {
 	// put your size call here
@@ -16,20 +16,76 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(66, 135, 245);	
   }
-
+ 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	
+	//ellipse(width/2, height/2, width, height);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //sand
+	  stroke(240,230,140);
+    fill(240,230,140);
+    rect(0,250, 400, 300);
+
+    //sand using variables
+    stroke(width/1.6,height/1.7,height/2.8);
+    fill(240,230,140);
+    rect(0,250, 400, 300);
+ /*   
+    //waves  
+    stroke(66, 135, 245);
+    fill(66, 135, 245);
+    ellipse(0,220, 400, 100);
+    
+    stroke(66, 135, 245);
+    fill(66, 135, 245);
+    ellipse(250,220, 400, 100);
+    
+    stroke(66, 135, 245);
+    fill(66, 135, 245);
+    ellipse(470,220, 400, 100);
+    
+//  grass
+    stroke(55, 135, 39);
+    fill(55, 135, 39);
+    bezier(400,200, 400,500,500, 500, 200, 400);
+    
+    
+//  sun
+    stroke(230, 255, 8);
+    fill(230, 255, 8);
+    ellipse(0, 0, 200, 200);  
+    
+    //tree
+    stroke(165,42,42);
+    fill(165,42,42);
+    rect(300,200,20,150);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(310,200,50,50);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(280,200,50,50);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(340,200,50,50);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(310,170,50,50);
+*/
+    
+    
+    
+   
+      
   }
   
   // define other methods down here.
